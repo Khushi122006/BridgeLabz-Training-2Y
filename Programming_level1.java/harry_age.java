4f75c81cee735e16d2f6d287b036d73fc58be910
Khushi122006 <khushi.pandey_cs24@gla.ac.in>
2025-08-19 15:20:15 +0530

    core-java

---
+++ Programming_level1.java/harry_age.java
@@ -0,0 +1,10 @@
+public class harry_age {
+    public static void main(String[] args) {
+        int current_year = 2024;
+        int birth_year = 2000;
+        int current_age = current_year - birth_year ; 
+        System.out.println("HARRY'S AGE IN 2024 IS : " + current_age);
+    }
+    
+}
+
